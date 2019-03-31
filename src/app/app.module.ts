e698982a561aff09a25324146005209b094b2af7
Abhishek Rawat <abhirawat.18@gmail.com>
2019-03-30 22:49:40 -0400

    1. Changes in home-page.component.html 2. Changes in home-page.component.ts 3. Changes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MainAgeSelectorComponent } from './main-age-selector/main-age-selector.component';
 import { DataService } from './data.service';
-import { HttpClientModule } from  '@angular/common/http';
+import { CustomQuizComponent } from './custom-quiz/custom-quiz.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    MainAgeSelectorComponent
+    MainAgeSelectorComponent,
+    CustomQuizComponent
   ],
   imports: [
     BrowserModule,
