222523a70687ac588e44d796cf7e5669f237168b
akshitaangara <35636606+akshitaangara@users.noreply.github.com>
2019-03-31 21:29:44 -0400

    error fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxLocalStorageModule } from 'ngx-localstorage';
-import { AlertsModule } from 'angular-alert-module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +26,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     NgxLocalStorageModule.forRoot(),
-    AlertsModule.forRoot(),
     HttpClientModule
   ],
   providers: [DataService],
